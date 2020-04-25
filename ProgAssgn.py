13e7d84f3a8b9b8add859ca0d00e000d36ac265f
Esther Pisano <estherpisano@Esthers-MBP.domain>
2020-04-24 18:48:23 -0700

    Completed Code. ASCII art

---
+++ ProgAssgn.py
@@ -0,0 +1,23 @@
+"""
+CTEC 121
+Esther Pisano
+First Programming Assignment
+ASCII Art assignment
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+
+def main():
+    print()
+    print("(\_/)")
+    print("( •,•)")
+    print("(\")_(\")")
+    print()
+
+
+main()
